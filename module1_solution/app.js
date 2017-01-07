6b9a0223d10f37f8819cd05481f8dae8e83b73b2
admin <admin>
2017-01-07 18:43:33 -0500

    new Folder

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -16,6 +16,8 @@ function LunchCheckController ($scope){
     // if the textbox is empty or has space char
     if (lunch.length === 0 || (lunch.length === 1 && lunch[0] === " ")) {
       $scope.output = "Please enter data first";
+      document.getElementById("lunch-menu").style.borderColor = "red"; //changing the borderColor
+      document.getElementById("output-massage").style.color = "red";
     }
     //textbox is not empty
     else{
@@ -27,7 +29,8 @@ function LunchCheckController ($scope){
         if (lunchList[i].length !== 0  || !lunchList[i].length === 1) {
           counter++;}
       };
-
+      document.getElementById("lunch-menu").style.borderColor = "green";
+        document.getElementById("output-massage").style.color = "green";
       //generating final output based on counter
       if (counter <= 3) {  $scope.output = "Enjoy!";}
       else { $scope.output = "Too much!";}
